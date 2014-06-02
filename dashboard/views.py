5ac81090981f2a969f269a5db0a04f42774b052b
Mariana <mary.buitres@gmail.com>
2014-06-02 20:03:48 -0300

    prueba git

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from student.models import Student
 
 # Create your views here.
 
 def lista(request):
-	render_to_response('index.html', context_instance = RequestContext(request))
+	
+	students = Student.objects.all()
+	return render_to_response('index.html',{'students':students}, context_instance = RequestContext(request))
+#	render_to_response('index.html', context_instance = RequestContext(request))
 
 def crear(request):
 	render_to_response('index.html', context_instance = RequestContext(request))
--- student/views.py
+++ student/views.py
@@ -1,3 +1,12 @@
+from eduaulas import settings
 from django.shortcuts import render
+from students.models import Student
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 # Create your views here.
+
+def listarAlumnos(request):
+
+	students = Student.objects.all()
+	return render_to_response('index.html',{'students':students}, context_instance = RequestContext(request))
