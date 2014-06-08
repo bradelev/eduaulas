5e6f27980e29bbf5872b182a05de89de2d1e0fb7
Mariana <mary.buitres@gmail.com>
2014-06-08 20:28:16 -0300

    panel

--- eduaulas/static/js/functions.js
+++ eduaulas/static/js/functions.js
@@ -15,8 +15,8 @@ function get_students_data () {
     type:'GET',
     dataType:"json",
     data:{
-          name:name,
-          last_name:last_name,
+          name:'name',
+          last_name:'last_name',
     }, 
 
     "success":function(data){
@@ -35,13 +35,21 @@ function create_table_students (data) {
 if(data['type'] == 'success'){  
 	 
     var output = "";
+    output+="<thead>";
+    output+="<tr>";
+    output+="<th>Nombre</th>";
+    output+="<th>Apellido</th>"
+    output+="</tr>"
+    output+="</thead>"
+    output+="<tbody >"
+
     for (var x in data["dictionary_students"]){
       output += "<tr>";
       (data["dictionary_students"][x]['name'] == "0") ? output += "<td></td>": output += "<td>"+data["dictionary_students"][x]['name']+"</td>";
       (data["dictionary_students"][x]['last_name'] == "0") ? output += "<td></td>": output += "<td>"+data["dictionary_students"][x]['last_name']+"</td>";
       output += "</tr>";
     }
-
+    output+="</tbody >"
     if (output != ""){
      
       $("#dt_alumnos").html(output);
