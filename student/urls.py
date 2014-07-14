c2f054bd446c848670e9caca9ffaa6ce361c9824
Mariana <mary.buitres@gmail.com>
2014-07-14 20:33:57 -0300

    Arreglo de url en views student

--- student/urls.py
+++ student/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
 
 	url(r'listas/(?P<code>\w+)?/?$', 'student.views.ini'),
 	url(r'listas/(?P<code>\w+)?/?/lista_cargar/?$', 'student.views.students_list'),
-	url(r'info_alumno/(?P<id>\w+)?/?$', 'student.views.student_info'),
+	url(r'info_alumno/(?P<id>\w+)/?$', 'student.views.student_info'),
    
 	
 )
