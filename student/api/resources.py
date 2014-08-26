e215baced0b8415b0fb946daec5a494cb59b6d69
Braulio De Leon <bradelev@hotmail.com>
2014-08-25 21:20:19 -0300

    tastypie en los modelos

--- student/api/resources.py
+++ student/api/resources.py
@@ -1,10 +1,14 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL
 from student.models import Student
+from tastypie import fields
 from tastypie.authorization import Authorization
 
 
 class StudentResource(ModelResource):
-    class Meta:
-    	authorization = Authorization()
-        queryset = Student.objects.all()
-        allowed_methods = ['post']
+	class Meta:
+		authorization = Authorization()
+		queryset = Student.objects.all()
+		allowed_methods = ['get','post']
+		filtering = {
+			'serial': ALL,
+		}
