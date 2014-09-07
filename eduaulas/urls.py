eb9479d62c6efac5a40ab95c1956083402aa6a35
Braulio De Leon <bradelev@hotmail.com>
2014-09-07 20:26:17 +0000

    urls.py edited online with Bitbucket

--- eduaulas/urls.py
+++ eduaulas/urls.py
@@ -53,4 +53,4 @@ urlpatterns = patterns('',
 
     
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 	
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
